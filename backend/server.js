51dc86f9d9c454c9e14165d8fec63ba9941613d1
tanzimrayhan <tanzim_rayhan@yahoo.com>
2020-02-12 20:06:48 -0500

    backend routes and schemas

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri=process.env.ATLAS_URI;
+//mongoose.connect(uri,{useNewUrlParser:true, useCreateIndex:true,useUnifiedTopology: true});
+
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(uri, {useNewUrlParser:true,useUnifiedTopology: true},(err, client)=> {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('MongoDB database connection established successfully...');
+   
+   client.close();
+});
+
+const exerciseRouter=require('./routes/exercise');
+const userRouter=require('./routes/users');
+
+app.use('/exercises',exerciseRouter);
+app.use('/users',userRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
