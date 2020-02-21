64391fcbd77446e97e53798c0721a80e683ef0c0
Tanzim Ali <tanzim_rayhan@yahoo.com>
2020-02-21 01:05:24 -0500

    Added the create-user form

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -40,6 +39,23 @@ class CreateUser extends Component {
         return (
             <div className="App" style={{ height: '100%' }}>
                 <header className="App-header" >
+                    <div>
+                        <h3> Create New User</h3>
+                        <form onSubmit={this.onSubmit}>
+                            <div className="form-group">
+                                <label>Username: </label>
+                                <input type="text"
+                                required
+                                className="form-control"
+                                value={this.state.username}
+                                onChange={this.onChangeUsername}/>
+                            </div>
+                            <div className="form-group">
+                                <input type="submit" value="Create User" className="btn btn-primary"/>
+                            </div>
+                        </form>
+                    </div>
+
                 You are on CreateUser list page!
                 </header>
             </div>
